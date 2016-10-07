a9526f2407c55fc3bbad9ed6ad534d1171abf878
Detlef von der Thuesen <dthuesen@gmx.de>
2016-10-07 14:46:55 +0200

    change title

--- src/js/components/Header/Brand.js
+++ src/js/components/Header/Brand.js
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom";
 export default class Brand extends React.Component {
   render() {
     return ( 
+      <div>
         <h5><b>Brand componente</b></h5>
+      </div>
     );
   }
 }
