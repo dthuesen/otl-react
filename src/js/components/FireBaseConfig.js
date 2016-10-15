36658a0f075a555a1eaa5aabd6e2491deb2ba42b
Detlef von der Thuesen <dthuesen@gmx.de>
2016-10-15 20:38:17 +0200

    refactoring firebase init and config to FireBaseConfig.js

--- src/js/components/FireBaseConfig.js
+++ src/js/components/FireBaseConfig.js
@@ -1,39 +1,16 @@
-import React from "react";
 import * as firebase from 'firebase';
 
 // Firebase
-    // const fbconfig = {
-    //   apiKey: "AIzaSyBG6RABAtYE_59MiD9j2-L_Z4C5i3rJOZY",
-    //   authDomain: "otl-react.firebaseapp.com",
-    //   databaseURL: "https://otl-react.firebaseio.com",
-    //   storageBucket: "otl-react.appspot.com",
-    //   messagingSenderId: "531210504974"
-    // };
+const fbconfig = {
+  apiKey: "AIzaSyBG6RABAtYE_59MiD9j2-L_Z4C5i3rJOZY",
+  authDomain: "otl-react.firebaseapp.com",
+  databaseURL: "https://otl-react.firebaseio.com",
+  storageBucket: "otl-react.appspot.com",
+  messagingSenderId: "531210504974"
+};
 
-    // const fireb = firebase
-    //   .initializeApp(fbconfig)
-    //   .database();
+export const fireb = firebase
+  .initializeApp(fbconfig)
+  .database();
 
-    // const fb = fireb
-    //   .ref();
-  
-
-
-export default class FireBaseConfig extends React.Component {
-
-  
-  
-
-  render() {
-    const fbconfig = {
-      apiKey: "AIzaSyBG6RABAtYE_59MiD9j2-L_Z4C5i3rJOZY",
-      authDomain: "otl-react.firebaseapp.com",
-      databaseURL: "https://otl-react.firebaseio.com",
-      storageBucket: "otl-react.appspot.com",
-      messagingSenderId: "531210504974"
-    }
-    return fbconfig
-            
-  }
-    
-}
+export const fb = fireb.ref();
