5b7a5aee918b0dfebc455f80337f5cbdbeb61620
Detlef von der Thuesen <dthuesen@gmx.de>
2016-10-18 15:39:38 +0200

    some refactorings

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+
 export default class About extends React.Component {
+
     render() {
         return (
+            
             <div>
                 <div className="container">
                     <div className="row">
