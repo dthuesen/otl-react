5b1afde6fe660022e2a82c194ee9f0397e068f73
Detlef von der Thuesen <dthuesen@gmx.de>
2017-03-26 19:43:17 +0200

    Clean github push

--- src/js/components/FireBaseConfig.js
+++ src/js/components/FireBaseConfig.js
@@ -1,8 +1,10 @@
 import * as firebase from 'firebase';
 import { FBconfig } from './firebase-config-object'
 
+
 // FirebaseConfigObject
 export const fbconfig = FBconfig;
+
 
 export const fireb = firebase
   .initializeApp(fbconfig)
